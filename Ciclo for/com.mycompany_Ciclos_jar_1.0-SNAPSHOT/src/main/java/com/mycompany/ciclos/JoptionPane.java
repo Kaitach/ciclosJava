6c8d1fab7c20702d4dcaad64bb0c25bb3e17d6f2
Kaitach <104739161+Kaitach@users.noreply.github.com>
2022-11-13 10:53:36 -0300

    Add files via upload

---
+++ Ciclo for/com.mycompany_Ciclos_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/ciclos/JoptionPane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ciclos;
+
+/**
+ *
+ * @author fou-k
+ */
+class JoptionPane {
+
+    static String showInputDialog(String cuantos_elementos_tienes_que_poner) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
